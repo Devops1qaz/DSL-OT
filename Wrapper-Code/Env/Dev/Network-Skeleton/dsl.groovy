1e229da67b1847ca066e2d6d4f80a7048feff64d
Ishaan-Dev5 <ishaanaggarwal32@gmail.com>
2025-10-15 16:49:46 +0530

    Update dsl.groovy

--- Wrapper-Code/Env/Dev/Network-Skeleton/dsl.groovy
+++ Wrapper-Code/Env/Dev/Network-Skeleton/dsl.groovy
@@ -1,85 +1,43 @@
-// ---------- Folder Hierarchy ----------
-folder('OTMS') { description('OTMS parent folder') }
-folder('OTMS/Env') { description('All environments') }
-folder('OTMS/Env/Dev') { description('Dev environment') }
-folder('OTMS/Env/Dev/Applications') { description('Applications for Dev') }
-folder('OTMS/Env/Dev/Applications/Network-Skeleton') { description('Network Skeleton app in Dev') }
-folder('OTMS/Env/Dev/Applications/Network-Skeleton/Infra') { description('Network Skeleton infra deployment jobs for Dev') }
-
-// ---------- Global Vars ----------
-def repoUrl = 'https://github.com/Snaatak-Cloudops-Crew/IAC-Terraform-repo.git'
-def gitCredsId = 'sonal-github'          
-def slackCredsId = 'slack-notifications' 
-def awsCredsId = 'AWS-Sneha'
-
-// ---------- Components ----------
-def components = [
-  [id: 'network_skeleton', branch: 'SCRUM-505-divya-mishra', path: 'Wrappercode/Env/Dev/Network-Skeleton']
-]
-
-// ---------- Job Creation ----------
-components.each { c ->
-    pipelineJob("OTMS/Env/Dev/Applications/Network-Skeleton/Infra/${c.id}") {
-        description("Deploy ${c.id} - branch ${c.branch} for Dev environment")
-        parameters {
-            stringParam('ENV', 'dev', 'Environment to deploy')
-            stringParam('AWS_REGION', 'ap-southeast-1', 'AWS region')
-        }
-        definition {
-            cps {
-                script("""
-pipeline {
-  agent any
-  tools { terraform 'terraform' }
-
-  parameters {
-    string(name: 'ENV', defaultValue: 'dev', description: 'Environment')
-    string(name: 'AWS_REGION', defaultValue: 'ap-southeast-1', description: 'AWS region')
-  }
-
-  environment {
-    REPO_URL = '${repoUrl}'
-    COMPONENT_PATH = '${c.path}'
-    COMPONENT_NAME = '${c.id}'
-    AWS_DEFAULT_REGION = "\${AWS_REGION}"
-  }
-
-  stages {
-    stage('Checkout') {
-      steps {
-        git branch: '${c.branch}', url: '${repoUrl}', credentialsId: '${gitCredsId}'
-      }
-    }
-
-    stage('Terraform Apply') {
-      steps {
-        withCredentials([aws(credentialsId: '${awsCredsId}', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-          dir("\${COMPONENT_PATH}") {
-            sh '''
-              terraform init -reconfigure
-              terraform plan -out=tfplan
-              terraform apply -auto-approve tfplan
-            '''
-          }
-        }
-      }
-    }
-  }
-
-  post {
-    success {
-      echo ":white_check_mark: ${c.id} deployed successfully for ENV=\${ENV}"
-      slackSend(channel: '#jenkins-notify', message: ":white_check_mark: ${c.id} deployed successfully in Dev (\${ENV})", tokenCredentialId: '${slackCredsId}')
-    }
-    failure {
-      echo ":x: ${c.id} deployment failed for ENV=\${ENV}"
-      slackSend(channel: '#jenkins-notify', message: ":x: ${c.id} deployment FAILED in Dev (\${ENV})", tokenCredentialId: '${slackCredsId}')
-    }
-  }
+folder('terraform') {
+    displayName('Terraform')
+    description('Contains all Terraform infrastructure deployment jobs')
 }
-                """.stripIndent())
-                sandbox(true)
+
+folder('terraform/env') {
+    displayName('Env')
+    description('Environment specific deployments')
+}
+
+folder('terraform/env/dev') {
+    displayName('Dev')
+    description('Development environment deployments')
+}
+
+folder('terraform/env/dev/Network-Skeleton') {
+    displayName('Network-Skeleton')
+    description('Terraform network skeleton deployment')
+}
+
+pipelineJob("terraform/env/dev/Network-Skeleton/deploy") {
+    parameters {
+        stringParam('BRANCH_NAME', 'main', 'Git branch to use')
+        choiceParam('ACTION', ['apply', 'destroy'], 'Choose apply to create infra or destroy to remove it.')
+    }
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/Devops1qaz/Wrapper-Code.git')
+                        
+                    }
+                    branches('*/${BRANCH_NAME}')
+                }
             }
+            scriptPath("Env/Dev/Network-Skeleton/Jenkinsfile")
         }
     }
 }
+
+println "✔︎ Pipeline job created → terraform/env/dev/Network-Skeleton/deploy"
